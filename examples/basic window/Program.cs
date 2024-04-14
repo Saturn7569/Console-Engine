702372f97cc14309ab585a7d14795acec142e423
Saturn7569 <143340840+Saturn7569@users.noreply.github.com>
2024-04-14 13:03:02 -0400

    Create Program.cs

---
+++ examples/basic window/Program.cs
@@ -0,0 +1,31 @@
+// THIS FILE IS JUST TO COPY AND PASTE THIS IS NOT A PROJECT
+// to see how this works go to:
+// creating a basic window
+
+using System;
+
+using ConsoleEngine;
+using ConsoleEngine.EntityManagement;
+
+namespace Main {
+    public class Program {
+        public static void Main(string[] args) {
+            RenderGrid screen = new(5,5,'-');
+
+            screen.InitWindow("Basic Game Grid");
+
+            while (true) {
+                GameDraw(screen);
+
+                ConsoleKey inp = screen.CheckKeyPressed();
+                if (inp == ConsoleKey.E) break;
+            }
+
+            void GameDraw(RenderGrid screen) {
+                screen.Clear('-');
+                screen.DrawUI("This is a basic window");
+                screen.Render();
+            }
+        }
+    }
+}
